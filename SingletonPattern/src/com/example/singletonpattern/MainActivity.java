47fa8167efb7d11239e64c1e59811612df3ed671
llitfkitfk <llitfkitfk@gmail.com>
2014-05-07 17:36:40 +0800

    complete singleton pattern

---
+++ SingletonPattern/src/com/example/singletonpattern/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.singletonpattern;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		((Button) findViewById(R.id.button1)).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Singleton obj1 = Singleton.getInstance();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
